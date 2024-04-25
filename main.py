6da79a56dbe8226a31c7f69d45f05b7d1a377dc3
InsomniacGuy <rahulchandan1991@gmail.com>
2024-04-25 12:42:22 +0530

    Day 31: Add URL endpoints for all data and yearly.

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import pandas as pd
 # creating flask object / website object from  Flask class
 app = Flask(__name__)
 
-
 # variable = "Sip some chai and keep coding"
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
 stations = stations[["STAID", "STANAME                                 "]]
+
+
 #  When the user visits home url home() is called. So route is connected to home().
 # @ symbol means a decorator and it means it connects this method(route) to function home()
 # However, to render the web-page tutorial.html flask needs a directory called "templates" with html file in it
@@ -29,6 +30,29 @@ def about(station, date):
     return {
         "station": station,
         "date": date,
+        "temperature": temperature
+    }
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + f"{station.zfill(6)}" + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    #  We want to return the entire data here for this file. In API we usually return a list or a dictionary.
+    return df.to_dict(orient='records')
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yer_data(station, year):
+    filename = "data_small/TG_STAID" + f"{station.zfill(6)}" + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    # temperature = df.loc[df['    DATE'].str.startswith(year)].to_dict(orient='records')
+    temperature = df[df['    DATE'].str.startswith(year)].to_dict(orient='records')
+
+    return {
+        "station": station,
+        "year": year,
         "temperature": temperature
     }
 
